d05c5b7d7929f47a3a7de9d0d30a28287a7b7262
Taylor Kettle <tin.teapot@gmail.com>
2023-10-18 12:59:29 +1300

    removing ascii conversion

--- powershell/helpers/load_helpers.ps1
+++ powershell/helpers/load_helpers.ps1
@@ -1,6 +1,10 @@
 # Intended for this to be downloaded and run, which will then install the rest 
 # of the modules in this folder, if you need to update or add any helpers you
 # can add them here without breaking any of the paths other scripts are using
+#
+# Intended to be called like: 
+# iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/tangelo-services-org/ninja-rmm-pub/main/powershell/helpers/load_helpers.ps1'))
+
 
 $base_url = 'https://raw.githubusercontent.com/tangelo-services-org/ninja-rmm-pub/main/powershell/helpers'
 
@@ -9,5 +13,5 @@ $helper_files = @('check_installed.ps1')
 foreach ($file in $helper_files)
 {
     Write-Host "Sourcing $file..."
-    . ([Scriptblock]::Create((([System.Text.Encoding]::ASCII).getString((Invoke-WebRequest -Uri "$base_url/$file").Content))))
+    . ([Scriptblock]::Create((Invoke-WebRequest -Uri "$base_url/$file").Content))
 }
