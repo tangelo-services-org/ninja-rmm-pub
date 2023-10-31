5876b044508a053e5f03583af7907abe18d69816
Taylor Kettle <tin.teapot@gmail.com>
2023-10-31 15:19:00 +1300

    add uninstall helper

--- powershell/run_from_git.ps1
+++ powershell/run_from_git.ps1
@@ -13,7 +13,7 @@ function RunFromGit
     {
         # If you want to add more helpers, include their names here and upload them to the 
         # powershell/helpers/ folder for the public github repo
-        $helper_files = @('check_installed.ps1', 'set_env_var.ps1', 'set_reg_key.ps1')
+        $helper_files = @('check_installed.ps1', 'set_env_var.ps1', 'set_reg_key.ps1', 'uninstall_program.ps1')
         $base_url = "$github_raw_url/ninja-rmm-pub/main/powershell/helpers"
 
         foreach ($file in $helper_files)
