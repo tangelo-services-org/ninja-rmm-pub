76af74a079e60aa84b19db9d4338a8ed6fad7cd1
Taylor Kettle <tin.teapot@gmail.com>
2023-11-15 12:17:41 +1300

    improving check_installed matching diff format

--- powershell/helpers/check_installed.ps1
+++ powershell/helpers/check_installed.ps1
@@ -2,28 +2,55 @@ function CheckInstalled
 {
 
     param(
-        [Parameter(Mandatory = $true)][string]$softwareName,
-        [Parameter(Mandatory = $true)][AllowEmptyString()][string]$softwareVersion
+        # Because we use -like in the comparisons, wildcard searches are supported i.e. * and ? operators
+        # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7.3
+        [Parameter(Mandatory = $true)][string]$softwareName, 
+        [Parameter(Mandatory = $true)][AllowEmptyString()][string]$softwareVersion # If you want to match higher versions, end with ^ i.e. 1.0.2^
     )
-    
+
+    $matchGreater = $False
+    if (-not [string]::IsNullOrEmpty($softwareVersion))
+    {
+        $matchGreater = $softwareVersion[-1] -eq '^'
+        $softwareVersion = $softwareVersion.TrimEnd('^')     # Trim out the ^ at the end of the version string so it doesn't mess with comparisons
+    }
+
     Write-Host "Checking if $softwareName $softwareVersion is installed..."
     foreach ($hive in @('HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall', 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall' ))
     {
-        $item = Get-ChildItem -LiteralPath $hive | Get-ItemProperty | Where-Object { $_.DisplayName -eq $softwareName -and ($_.DisplayVersion -eq $softwareVersion -or [string]::IsNullOrEmpty($_.DisplayVersion)) }
-        if ($item)
+        $item = Get-ChildItem -LiteralPath $hive | Get-ItemProperty | Where-Object { $_.DisplayName -like $softwareName }
+
+        # Exit if there is no version specified
+        if ([string]::IsNullOrEmpty($softwareVersion))
+        {
+            Write-Host "$softwareName (no version) already installed" 
+            Return 0
+        }
+        
+        # Match the display version exactly
+        if ($item.DisplayVersion -like $softwareVersion)
         {
             Write-Host "$softwareName $softwareVersion already installed" 
             Return 0
         }
+
+        # Try casting the version to a [version] and then if requested matching upwards
+        try
+        {
+            if ($matchGreater -and ([version]$item.DisplayVersion -ge [version]$softwareVersion))
+            {
+                Write-Host "$softwareName $($item.DisplayVersion)  already installed" 
+            }
+        }
+        catch [System.Management.Automation.RuntimeException]
+        {
+            Write-Host "Error converting to [version] $_"
+        }     
+
+        # TODO: matching on the hidden version number for comparisons etc, this is: $item.Version
+      
     }
 
-    # Check if the software is installed, if it is - exit the script
-    # $software = Get-WmiObject -Query "SELECT * FROM Win32_Product WHERE Name='$softwareName' AND Version='$softwareVersion'"
-    # if ( $software )
-    # {
-    #     Write-Host "$softwareName $softwareVersion already installed, exiting..." 
-    #     Return 0
-    # }
     Write-Host "$softwareName $softwareVersion not installed"
     Return 1
 }
