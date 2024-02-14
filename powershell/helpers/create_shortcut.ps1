0ee232cfee95c8b38a43a2ec5fd849b40d2fc67e
Taylor Kettle <tin.teapot@gmail.com>
2024-02-14 17:56:27 +1300

    handle working dir

--- powershell/helpers/create_shortcut.ps1
+++ powershell/helpers/create_shortcut.ps1
@@ -3,13 +3,21 @@ function CreateShortcut
 {
     param (
         [Parameter(Mandatory = $true)][string]$shortcutPath,
-        [Parameter(Mandatory = $true)][string]$targetPath
+        [Parameter(Mandatory = $true)][string]$targetPath,
+        [string]$workingDir
     )
+
+    if (-not $workingDir)
+    {
+        $workingDir = Split-Path $targetPath -Parent
+    }
+
     LogWrite "Creating shortcut at $shortcutPath to $targetPath"
     New-Item -ItemType Directory -Path "$([System.IO.Path]::GetDirectoryName($shortcutPath))" -Force -ErrorAction SilentlyContinue
 
     $shell = New-Object -ComObject WScript.Shell
     $shortcut = $shell.CreateShortcut($shortcutPath)
     $shortcut.TargetPath = $targetPath
+    $shortcut.WorkingDirectory = $workingDir
     $shortcut.Save()
 }
